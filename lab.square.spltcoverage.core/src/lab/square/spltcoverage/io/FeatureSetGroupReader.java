ca2e92210a8f8e03467eb58071e55c89c908586d
unknown <hanidiot@gmail.com>
2022-05-23 20:39:17 +0900

    now FeatureSetGroupReader can read also .xml config files. FeatureSetGroupReader test case updated to be testable

--- lab.square.spltcoverage.core/src/lab/square/spltcoverage/io/FeatureSetGroupReader.java
+++ lab.square.spltcoverage.core/src/lab/square/spltcoverage/io/FeatureSetGroupReader.java
@@ -9,6 +9,16 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 /**
  * 
  * @author selab
@@ -33,23 +43,77 @@ public class FeatureSetGroupReader implements IFeatureSetReader {
 		Collection<Map<String, Boolean>> products = new LinkedList<Map<String, Boolean>>();
 
 		for (File productFolder : groupFolder.listFiles()) {
-			Map<String, Boolean> featureSet = new HashMap<String, Boolean>();
-			File featureSetFile = new File(productFolder.listFiles()[0].getAbsolutePath());
-			BufferedReader reader = null;
-			try {
-				reader = new BufferedReader(new FileReader(featureSetFile));
-				String line = null;
-				while((line = reader.readLine()) != null){
-					featureSet.put(line, true);
-				}
-				reader.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+			File featureSetFile = findFeatureSetFile(productFolder);
+			if(featureSetFile == null)
+				continue;
+
+			Map<String, Boolean> featureSet = readFeatureSet(featureSetFile);
+			
 			products.add(featureSet);
 		}
 
 		return products;
+	}
+
+	private Map<String, Boolean> readFeatureSet(File featureSetFile) {
+		Map<String, Boolean> featureSet = new HashMap<String, Boolean>();
+		if(isOlderVersionFeatureSet(featureSetFile.getName())) {
+			readFeatureSetOld(featureSetFile, featureSet);
+		}
+		else {
+			readFeatureSetNew(featureSetFile, featureSet);
+		}
+		return featureSet;
+	}
+
+	private void readFeatureSetNew(File featureSetFile, Map<String, Boolean> featureSet) {
+		try {
+			DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			Document document = db.parse(featureSetFile);
+			NodeList featureList = document.getElementsByTagName("feature");
+			for (int i = 0; i < featureList.getLength(); i++) {
+				Node feature = featureList.item(i);
+				if(feature.getNodeType() == Node.ELEMENT_NODE) {
+					Element element = (Element)feature;
+					String name = element.getAttribute("name");
+					String selected = element.getAttribute("manual");
+					featureSet.put(name, selected.equalsIgnoreCase("selected"));
+				}
+			}
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void readFeatureSetOld(File featureSetFile, Map<String, Boolean> featureSet) {
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new FileReader(featureSetFile));
+			String line = null;
+			while((line = reader.readLine()) != null){
+				featureSet.put(line, true);
+			}
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private boolean isOlderVersionFeatureSet(String name) {
+		return name.toUpperCase().endsWith(".CONFIG");
+	}
+
+	private File findFeatureSetFile(File productFolder) {
+		for (File file : productFolder.listFiles()) {
+			if(isOlderVersionFeatureSet(file.getName())) {
+				return file;
+			}
+			// newer
+			if(file.getName().toUpperCase().endsWith(".XML")) {
+				return file;
+			}
+		}
+		return null;
 	}
 
 }
--- lab.square.spltcoverage.core/src/lab/square/spltcoverage/test/FeatureSetGroupReaderTest.java
+++ lab.square.spltcoverage.core/src/lab/square/spltcoverage/test/FeatureSetGroupReaderTest.java
@@ -1,25 +1,88 @@
 package lab.square.spltcoverage.test;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.Collection;
 import java.util.Map;
 
 import org.junit.Test;
 
 import lab.square.spltcoverage.io.FeatureSetGroupReader;
+import lab.square.spltcoverage.test.target.Configuration;
 
 public class FeatureSetGroupReaderTest {
 
 	@Test
-	public void testRead() {
+	public void testReadOlder() {
 		FeatureSetGroupReader reader = new FeatureSetGroupReader(
-				"D:\\workspacechallenege\\challenge-master\\workspace_IncLing\\Tools\\All_valid_conf\\FeatureAMP2\\products");
+				"D:\\workspace_experiment_challenge\\lab.square.spltcoverage.core\\src\\lab\\square\\spltcoverage\\test\\target\\productsOlder");
 		Collection<Map<String, Boolean>> products = reader.readAll();
 		
+		int configSum = 0;
+		int numConfigA = 0;
+		int numConfigB = 0;
+		int numConfigC = 0;
+		
 		for(Map<String, Boolean> featureSet : products) {
-			for(String feature : featureSet.keySet())
+			for(String feature : featureSet.keySet()) {
+				if(!featureSet.get(feature))
+					continue;
+				if(feature.equalsIgnoreCase(Configuration.CONFIG_A)) {
+					numConfigA++;
+				}
+				if(feature.equalsIgnoreCase(Configuration.CONFIG_B)) {
+					numConfigB++;
+				}
+				if(feature.equalsIgnoreCase(Configuration.CONFIG_C)) {
+					numConfigC++;
+				}
+				configSum++;
 				System.out.println(feature);
+			}
 			System.out.println("======================");
 		}
 		
+		assertEquals(5, products.size());
+		assertEquals(7, configSum);
+		assertEquals(3, numConfigA);
+		assertEquals(3, numConfigB);
+		assertEquals(1, numConfigC);
+	}
+	
+	@Test
+	public void testReadNewer() {
+		FeatureSetGroupReader reader = new FeatureSetGroupReader(
+				"D:\\workspace_experiment_challenge\\lab.square.spltcoverage.core\\src\\lab\\square\\spltcoverage\\test\\target\\productsNewer");
+		Collection<Map<String, Boolean>> products = reader.readAll();
+		
+		int configSum = 0;
+		int numConfigA = 0;
+		int numConfigB = 0;
+		int numConfigC = 0;
+		
+		for(Map<String, Boolean> featureSet : products) {
+			for(String feature : featureSet.keySet()) {
+				if(!featureSet.get(feature))
+					continue;
+				if(feature.equalsIgnoreCase(Configuration.CONFIG_A)) {
+					numConfigA++;
+				}
+				if(feature.equalsIgnoreCase(Configuration.CONFIG_B)) {
+					numConfigB++;
+				}
+				if(feature.equalsIgnoreCase(Configuration.CONFIG_C)) {
+					numConfigC++;
+				}
+				configSum++;
+				System.out.println(feature);
+			}
+			System.out.println("======================");
+		}
+		
+		assertEquals(5, products.size());
+		assertEquals(7, configSum);
+		assertEquals(3, numConfigA);
+		assertEquals(3, numConfigB);
+		assertEquals(1, numConfigC);
 	}
 }
