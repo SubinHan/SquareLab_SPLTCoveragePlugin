2b8ff3efb081621ec6edcdab473167b0149c3503
unknown <hanidiot@gmail.com>
2022-05-24 12:02:39 +0900

    LinkerTest2 improved.

--- lab.square.spltcoverage.core/src/lab/square/spltcoverage/test/LinkerTest2.java
+++ lab.square.spltcoverage.core/src/lab/square/spltcoverage/test/LinkerTest2.java
@@ -1,16 +1,20 @@
 package lab.square.spltcoverage.test;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import lab.square.spltcoverage.core.analysis.ProductLinker;
 import lab.square.spltcoverage.io.FeatureSetGroupReader;
 import lab.square.spltcoverage.model.ProductGraph;
+import lab.square.spltcoverage.test.target.Configuration;
 
 /*
  * Test 시: core의 plugin.xml dependency -> org.jacoco (0.8.6) 이어야 함.
@@ -18,15 +22,45 @@ import lab.square.spltcoverage.model.ProductGraph;
  */
 public class LinkerTest2 {
 
-	private Collection<ProductGraph> visited = new ArrayList<ProductGraph>();
+	private Collection<ProductGraph> visited;
+	
+	private Collection<Map<String,Boolean>> expected;
 
+	@Before
+	public void setUp() {
+		visited = new ArrayList<ProductGraph>();
+		expected = new ArrayList<>();
+		
+		Map<String, Boolean> featureSet1 = new HashMap<>();
+		Map<String, Boolean> featureSet2 = new HashMap<>();
+		Map<String, Boolean> featureSet3 = new HashMap<>();
+		Map<String, Boolean> featureSet4 = new HashMap<>();
+		Map<String, Boolean> featureSet5 = new HashMap<>();
+		
+		featureSet2.put(Configuration.CONFIG_A, true);
+		
+		featureSet3.put(Configuration.CONFIG_B, true);
+		
+		featureSet4.put(Configuration.CONFIG_A, true);
+		featureSet4.put(Configuration.CONFIG_B, true);
+		
+		featureSet5.put(Configuration.CONFIG_A, true);
+		featureSet5.put(Configuration.CONFIG_B, true);
+		featureSet5.put(Configuration.CONFIG_C, true);
+		
+		expected.add(featureSet1);
+		expected.add(featureSet2);
+		expected.add(featureSet3);
+		expected.add(featureSet4);
+		expected.add(featureSet5);
+	}
+	
 	@Test
 	public void testLinkerWithOnlyFeatureSets() {
 		String directory;
 		directory = "D:\\workspace_experiment_challenge\\lab.square.spltcoverage.core\\src\\lab\\square\\spltcoverage\\test\\target\\productsNewer";
 
 		testLinker(directory);
-
 	}
 
 	private void testLinker(String directory) {
@@ -37,6 +71,7 @@ public class LinkerTest2 {
 		if (heads.isEmpty())
 			fail();
 
+		
 		for (ProductGraph head : heads)
 			visitGraphRecur(head);
 	}
@@ -58,12 +93,48 @@ public class LinkerTest2 {
 			System.out.print("  ");
 			printFeatures(parent.getFeatureSet());
 		}
+		
+		assertTrue(contains(expected, graph.getFeatureSet()));
 
 		for (ProductGraph child : graph.getChildren()) {
 			visitGraphRecur(child);
 		}
 	}
 
+
+	private boolean contains(Collection<Map<String, Boolean>> featureSets, Map<String, Boolean> target) {
+		for(Map<String, Boolean> featureSet : featureSets) {
+			boolean flag = true;
+			for(String key : featureSet.keySet()) {
+				if(!equals(featureSet.get(key), target.get(key)))
+					flag = false;
+			}
+			for(String key : target.keySet()) {
+				if(!equals(featureSet.get(key), target.get(key)))
+					flag = false;
+			}
+			if(flag)
+				return true;
+		}
+		
+		return false;
+	}
+
+	private boolean equals(Boolean boolean1, Boolean boolean2) {
+		if(boolean1 == null) {
+			if(boolean2 == null)
+				return true;
+			if(boolean2 == false)
+				return true;
+		}
+		if(boolean2 == null) {
+			if(boolean1 == null)
+				return true;
+			if(boolean2 == false)
+				return true;
+		}
+		return boolean1 == boolean2;
+	}
 
 	private void printFeatures(Map<String, Boolean> featureSet) {
 		for (String key : featureSet.keySet()) {
