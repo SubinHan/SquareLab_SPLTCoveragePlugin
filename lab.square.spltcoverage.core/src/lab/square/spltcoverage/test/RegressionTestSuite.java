94981ba1eb6d252cb8971839ad6248232eb53206
unknown <hanidiot@gmail.com>
2022-07-07 11:46:25 +0900

    FeatureExpressionParserTest added in RegressionTestSuite

--- lab.square.spltcoverage.core/src/lab/square/spltcoverage/test/RegressionTestSuite.java
+++ lab.square.spltcoverage.core/src/lab/square/spltcoverage/test/RegressionTestSuite.java
@@ -5,7 +5,8 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({CoverageGeneratorTest2.class, CoverageReaderTest.class, LinkerTest2.class, 
-	FeatureSetGroupReaderTest.class, SplCoverageReaderTest.class, AntennaCoverageGeneratorTest.class, FeatureLocatorTest.class})
+	FeatureSetGroupReaderTest.class, SplCoverageReaderTest.class, AntennaCoverageGeneratorTest.class, FeatureLocatorTest.class,
+	FeatureExpressionParserTest.class})
 public class RegressionTestSuite {
 
 }
