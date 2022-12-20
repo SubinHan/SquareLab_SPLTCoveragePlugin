a6cab235f4eafaee2d6a99f20a953e59b54154ec
수빈 한 <hangenius123@naver.com>
2022-12-20 17:10:00 +0900

    added comment in Ex8

--- lab.square.spltcoverage.example/src/lab/square/spltclient/example/Ex8AntennaFeatureCoverage.java
+++ lab.square.spltcoverage.example/src/lab/square/spltclient/example/Ex8AntennaFeatureCoverage.java
@@ -20,19 +20,26 @@ public class Ex8AntennaFeatureCoverage {
 		
 		Collection<ProductCoverage> pcs = splCoverage.getProductCoverages();
 		
+		// We can get information of feature coverage about single feature, or multiple features.
+		// In this case, I'm interseted in "FloorPermission" Feature.
 		FeatureSet targetFeatureSet = new FeatureSet();
 		targetFeatureSet.addFeature(TARGET_FEATURE);
 		
 		for(ProductCoverage pc : pcs) {
 			if(!(pc instanceof AntennaProductCoverage))
 				continue;
+			// If correspond product doesn't have "FloorPermission", then continue.
 			if(!pc.getFeatureSet().hasFeature(TARGET_FEATURE))
 				continue;
 			
 			AntennaProductCoverage apc = (AntennaProductCoverage)pc;
 			
+			// We can analysis by using AntennaProductAnalysis
+			// by just passing an AntennaProduct object.
 			AntennaProductAnalysis analysis = new AntennaProductAnalysis(apc);
 			
+			// We can get collection of FeatureCoverages
+			// that contains only about given target feature set.
 			Collection<FeatureCoverage> featureCoverages = analysis.getFeatureCoverage(targetFeatureSet);
 			
 			int totalLines = 0;
